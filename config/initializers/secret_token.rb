0890412cefdbeafb8954511f21415d68e0be4597
Shaan Sapra <ssapra@uchicago.edu>
2012-12-15 13:36:44 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linguamocha::Application.config.secret_token = '3f189cd181c1e3f93c24ed3cb65ce7252781faba49bbcab3bd30c7bdb16684db8a8f15c67fa8d11a45d31a4c36f7d04034415453f7342c349cdb34b57345592d'

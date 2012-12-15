5831c2c32a52592ba49b461564e729c0cde0519f
Shaan Sapra <ssapra@uchicago.edu>
2012-12-15 13:46:27 -0600

    added devise. user model. dashboard controller

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module Linguamocha
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
+    
+    config.assets.initialize_on_precompile = false
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,

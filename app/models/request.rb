dd096dd0cd7c71dd28738c0dbc8b979b30ad0422
Shaan Sapra <ssapra@uchicago.edu>
2012-12-22 22:46:10 -0600

    api info saved

--- app/models/request.rb
+++ app/models/request.rb
@@ -16,4 +16,21 @@ class Request < ActiveRecord::Base
   def last_reply
     Message.find_all_by_request_id(self.id).last.user
   end
+  
+  
+  # Google Maps API key: AIzaSyDFXdzd98ckgjQL-ix-2GedGRVvGl9ef-U
+  
+  # Yelp API v1.0 key: yrOiKALTY0Mw_wBYJVmPwg
+  
+  # API v2.0
+  # Consumer Key  EcxkaDnICiYJE2PZm_OeCw
+  # Consumer Secret Yqc_OOMULadnzTs7Juj8bmZN3Ng
+  # Token sKXNbkV6k17LqXx3Rjl21BzvpP26O9CF
+  # Token Secret  8EuUtxYMsqGD89qtIRkDj-LOXKg
+  
+  # https://developers.google.com/maps/documentation/javascript/tutorial
+  
+  
+  
+  
 end

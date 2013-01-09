528ed84e94d0601b1d991129756922ef492424f7
Shaan Sapra <ssapra@uchicago.edu>
2013-01-09 16:07:39 -0600

    ugh

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,12 +33,12 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :my_skills, :allow_destroy => true
   accepts_nested_attributes_for :interests, :allow_destroy => true
   
-  searchable do
-    text :name, :username
-    text :my_skills do
-      my_skills.map { |skill| skill.tag }
-    end
-  end
+  # searchable do
+  #     text :name, :username
+  #     text :my_skills do
+  #       my_skills.map { |skill| skill.tag }
+  #     end
+  #   end
   
   def sent_requests
     Request.find_all_by_sender_id(self.id)

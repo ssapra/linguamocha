24bef51ec7cfb3413398eb924c50cd1e7aa7cf25
Shaan Sapra <ssapra@uchicago.edu>
2013-02-24 17:08:15 -0600

    edit, update, and delete working for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -18,4 +18,41 @@ class SkillsController < ApplicationController
 	  end
 	end
 
+
+	def edit
+		@skill = MySkill.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
+	end
+
+	def update
+		@skill = MySkill.find(params[:id])
+    
+    respond_to do |format|
+      if @skill.update_attributes(params[:my_skill])
+        format.html { redirect_to @skill, notice: 'Skill was successfully updated.' }
+        format.json { head :no_content }
+        format.js
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @skill.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+	def destroy
+		@skill = MySkill.find(params[:id])
+    @skill.destroy
+
+    respond_to do |format|
+      format.html { redirect_to current_user }
+      format.json { head :no_content }
+      format.js
+    end
+
+	end
+
 end
